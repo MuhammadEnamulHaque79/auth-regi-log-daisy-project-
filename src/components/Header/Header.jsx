3f0c33f7a3c18e88fc36074b909c32bd28adf8ff
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-04-18 11:41:50 +0600

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,43 @@
 import React, { useContext } from "react";
+import "./Header.css";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProviders";
 
-
-
 const Header = () => {
-    const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
-    <div>
-      <div className="navbar bg-neutral text-neutral-content">
-        <Link className="btn btn-ghost normal-case text-xl" to="/">Dream Hub</Link>{''}
-       
-        <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>{''}
-        <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>{''}
-        <Link className="btn btn-ghost normal-case text-xl" to="/register">Register</Link>
-        {
-          user ? <span>{user.email}</span> : <button className="btn btn-xs">sign out</button>
-        }
-      </div>
+    <>
+      <div className="navbar bg-neutral text-neutral-content nav-container">
+        <div className="nav-left-part">
+        <Link className="btn btn-ghost normal-case text-xl" to="/">
+          Dream Hub
+        </Link>
+        <div className="user-info">
+          {user ? (
+            <span>{user.email}</span>
+          ) : (
+            <button className="btn btn-xs">sign out</button>
+          )}
+        </div>
+        </div>
 
-    </div>
+        <div className="nav-right-part">
+          <Link className="btn btn-ghost normal-case text-xl" to="/">
+            Home
+          </Link>
+          {""}
+          <Link className="btn btn-ghost normal-case text-xl" to="/login">
+            Login
+          </Link>
+          {""}
+          <Link className="btn btn-ghost normal-case text-xl" to="/register">
+            Register
+          </Link>
+        </div>
+
+      </div>
+    </>
   );
 };
 
